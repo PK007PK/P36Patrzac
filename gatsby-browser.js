8243b080625f24e5d9001534bad0ac289d282aea
Piotr Krasny <krasny.piotr@gmail.com>
2021-01-10 20:39:27 +0100

    Initial commit

---
+++ gatsby-browser.js
@@ -0,0 +1,14 @@
+// custom typefaces
+// import "typeface-montserrat"
+// import "typeface-merriweather"
+// normalize CSS across browsers
+// import "./src/normalize.css"
+// custom CSS styles
+// import "./src/style.css"
+
+// Highlighting for code blocks
+// import "prismjs/themes/prism.css"
+
+import "@fortawesome/fontawesome-free/scss/fontawesome.scss";
+import "./src/scss/mdb/mdb.free.scss";
+import "./src/scss/mdb/bootstrap/bootstrap.scss";
